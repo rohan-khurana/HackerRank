cb0b001e25974333ba1d427668748bc45806212b
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-24 17:48:31 +0530

    Create Day 10: Binary Numbers.cpp
    Day 10: Binary Numbers

---
+++ 30 Days of Code/Day 10: Binary Numbers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int count = 0, maxCount = 0;
+    while (n)
+    {
+        int rem = n % 2;        
+        count = (count + rem) * rem;
+        maxCount = max(count, maxCount);        
+        n>>=1;
+    }
+    cout << maxCount << endl;
+    return 0;
+}
