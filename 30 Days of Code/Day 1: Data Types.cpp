3716e5bd414cefd986d445ede7c023eaf196bc4a
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-24 17:08:23 +0530

    Create Day 1: Data Types.cpp
    Day 1: Data Types

---
+++ 30 Days of Code/Day 1: Data Types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    int j;
+    double d2;
+    string s2;
+    cin>>j;
+    cin>>d2;
+    getline(cin >> ws, s2);
+    s=s+s2;
+    d2=d2+d;
+    cout<<i+j<<endl;
+    printf("%.1f\n",d2);
+    cout<<s;
+    return 0;
+}
