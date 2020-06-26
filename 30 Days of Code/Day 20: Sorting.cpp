d35aa878dba818f262d70d5dfa43168e74e9ae50
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-26 18:07:08 +0530

    Create Day 20: Sorting.cpp
    Day 20: Sorting

---
+++ 30 Days of Code/Day 20: Sorting.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    int numSwaps = 0;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0; a_i < n; a_i++){
+    	cin >> a[a_i];
+    }
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - 1; j++) {
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+            numSwaps++;
+        }
+    }
+    }
+    cout<<"Array is sorted in "<<numSwaps<<" swaps."<<endl;
+    cout<<"First Element: "<<a[0]<<endl<<"Last Element: "<<a[n-1];
+    return 0;
+}
