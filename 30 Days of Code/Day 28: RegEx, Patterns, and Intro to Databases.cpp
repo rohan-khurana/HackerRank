2bb164b39bf22066011cfb38ae1d9c7e014818a8
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-28 17:49:00 +0530

    Create Day 28: RegEx, Patterns, and Intro to Databases.cpp
    Day 28: RegEx, Patterns, and Intro to Databases

---
+++ 30 Days of Code/Day 28: RegEx, Patterns, and Intro to Databases.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    regex e(".+@gmail\\.com$");
+    vector<string> db;
+    for(int i = 0; i < N; i++){
+        string firstName;
+        string emailID;
+        cin >> firstName >> emailID;
+        if(regex_match(emailID,e))
+        {
+            db.push_back(firstName);
+        }
+    }
+    sort(db.begin(),db.end()); 
+    for(int i = 0;i < db.size();i++)
+    {
+        cout << db[i] << endl; 
+    }
+
+    return 0;
+
+}
